75106a90115489f77f510a017e6389fd1e5ea84b
Jae <daniel.kwong.h@gmail.com>
2020-06-25 17:51:05 -0700

    updates the message look

--- index.js
+++ index.js
@@ -14,10 +14,13 @@ bot.on('ready', () => {
 })
 
 let scheduledMessage = new cron.CronJob('00 55 5,12,14,15,16  * * *', () => {
-    bot.channels.cache.get(channelID).send("@everyone" + " Flag Race will begin in 5 minutes!");
+    bot.channels.cache.get(channelID).send("@everyone" + " :triangular_flag_on_post: Flag Race will begin in **_5 minutes_**! :triangular_flag_on_post:");
 });
 
 bot.on('message', msg=> {
+    // if (msg.content == "announcement") {
+    //     bot.channels.cache.get(channelID).send("@everyone" + " :triangular_flag_on_post: Flag Race will begin in **_5 minutes_**! :triangular_flag_on_post:");
+    // }
     if (msg.content === "!flag") {
         msg.reply({embed: {
             color: 3447003,
@@ -25,11 +28,11 @@ bot.on('message', msg=> {
               name: bot.user.username,
               icon_url: bot.user.avatarURL
             },
-            title: "Flag Race Times",
+            title: ":triangular_flag_on_post: Flag Race Times :triangular_flag_on_post:",
             // url: "http://google.com",
             description: "I will announce flag race 5 minutes before each time in the flag announcement channel!",
             fields: [{
-                name: "Please attend our flag races!",
+                name: "Please attend our flag races, we get points even if you just AFK!",
                 value: "**__PDT (UTC -7): 5:00 AM, 12:00 PM, 2:00 PM, 3:00 PM, and 4:00 PM\n" +
                 "EDT (UTC -4): 8:00 AM, 3:00 PM, 5:00 PM, 6:00 PM, and 7:00 PM\n" +
                "CEST (UTC +2): 2:00 PM, 9:00 PM, 11:00 PM, 12:00 AM, and 1:00 AM\n" +
