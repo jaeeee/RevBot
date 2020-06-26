6803e6e7f58e60bc9cbd0e52feb3f03dc5971368
Jae <daniel.kwong.h@gmail.com>
2020-06-25 17:19:58 -0700

    adds config file

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
+const config = require('config');
 const Discord = require('discord.js');
 const cron = require('cron');
+// const configuration = require('config.js');
 
 const bot = new Discord.Client();
 
-const token = '';
+let token = config.get('token.key');
+
+const channelID = '725851446511272029';
 
 bot.on('ready', () => {
     console.log("bot is up");
-    bot.channels.cache.get("725851446511272029").send("@everyone" + " Flag Race will begin in 5 minutes!");
-    // bot.channels.cache.get("725851446511272029").send("yo");
-    // console.log(bot.channels);
+    // bot.channels.cache.get(channelID).send("@everyone" + " Flag Race will begin in 5 minutes!");
 })
 
-bot.on('message', msg=> {
-    if(msg.content === "HELLO") {
-        msg.reply("HI BB");
-    }
-})
-
-// 5 AM
+let scheduledMessage = new cron.CronJob('00 55 5,12,14,15,16  * * *', () => {
+    // 5 AM
 // 12 PM
 // 2 PM 
 // 3 PM 
 // 4 PM
-
-let scheduledMessage = new cron.CronJob('00 55 05,12,14,15,16  * * *', () => {
-    console.log("hello");
-    bot.channels.cache.get("725851446511272029").send("@everyone" + " Flag Race will begin in 5 minutes!");
+    // console.log("hello");
+    bot.channels.cache.get(channelID).send("@everyone" + " Flag Race will begin in 5 minutes!");
 });
 
 bot.on('message', (message) => {
