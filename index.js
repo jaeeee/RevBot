6fe312a86238c55b404b44f1f34516c08f79871e
Jae <daniel.kwong.h@gmail.com>
2021-08-10 00:29:09 -0400

    revbot -> jaebot. configured for boundless

--- index.js
+++ index.js
@@ -10,16 +10,19 @@ const bot = new Discord.Client({
 
 let token = config.get('token.key');
 
-const channelID = '725873677278773320';
-const botChannelID = '504562059279728640';
+const channelID = '874506235003695145'; // channel id for flag race notifications
+const botChannelID = '194356307128483841'; // channel for bot replies
+// const dailyMessageChannelID = '199218706029608961'; // channel for daily messages
 
+//<@&725880768659980330>
+const flagRaceRoleID = '<@&844347624500232222>'; // flag race role id
 
 bot.on('ready', () => {
   // bot.off();
   console.log("bot is up");
   // bot.user.setStatus("Flag Race announcement bot. Use !flag for flag race times.");
-  bot.user.setActivity("roast beef");
-  // bot.channels.cache.get("199218706029608961").send("rawr");
+  bot.user.setActivity("flag race notifications");
+  // bot.channels.cache.get(dailyMessageChannelID).send("rawr");
   // bot.channels.cache.get(channelID).send("<@&725880768659980330>" + " :triangular_flag_on_post: Flag Race will begin in **_5 minutes_**! :triangular_flag_on_post:");
   // bot.user.setStatus("slave to Revenance");
   // bot.user.setPresence("");
@@ -36,49 +39,53 @@ bot.on('ready', () => {
 let flagMessage = new cron.CronJob('55 11,18,20,21,22 * * *', () => {
   // console.log("fire now (2)");
   console.log("Sending Flag Race announcement");
-  bot.channels.cache.get(channelID).send("<@&725880768659980330>" + " :triangular_flag_on_post: Flag Race will begin in **_5 minutes_**! :triangular_flag_on_post:");
+  bot.channels.cache.get(channelID).send("" + flagRaceRoleID + " :triangular_flag_on_post: Flag Race will begin in **_5 minutes_**! :triangular_flag_on_post:");
 }, null, true, 'UTC');
 
-let dailyMessage = new cron.CronJob('0 0 * * *', () => {
-  // console.log("fire now (2)");
-  console.log("Sending daily message");
-  bot.channels.cache.get("199218706029608961").send("Hello everyone, happy reset! This is just a daily reminder to check in and GP cap. Thank you!");
-}, null, true, 'UTC');
+// let dailyMessage = new cron.CronJob('0 0 * * *', () => {
+//   // console.log("fire now (2)");
+//   console.log("Sending daily message");
+//   bot.channels.cache.get(dailyMessageChannelID).send("Hello everyone, happy reset! This is just a daily reminder to check in and GP cap. Thank you!");
+// }, null, true, 'UTC');
 
-let gpqMessage = new cron.CronJob('0 22 * * 5', () => {
-  // console.log("fire now (2)");
-  console.log("Sending GPQ message");
-  bot.channels.cache.get("199218706029608961").send("Hello everyone, we will be starting Guild PQ in **_1 hour_**. See you all there at Guild HQ Channel 4 (via the Lea NPC in Henesys)!");
-});
+// let gpqMessage = new cron.CronJob('0 22 * * 5', () => {
+//   // console.log("fire now (2)");
+//   console.log("Sending GPQ message");
+//   bot.channels.cache.get(dailyMessageChannelID).send("Hello everyone, we will be starting Guild PQ in **_1 hour_**. See you all there at Guild HQ Channel 4 (via the Lea NPC in Henesys)!");
+// });
 
-let gpqMessage2 = new cron.CronJob('30 22 * * 5', () => {
-  // console.log("fire now (2)");
-  console.log("Sending GPQ (2) message");
-  bot.channels.cache.get("199218706029608961").send("Hello everyone, we will be starting Guild PQ in **_30 minutes_**! See you all there at Guild HQ Channel 4 (via the Lea NPC in Henesys)!");
-});
+// let gpqMessage2 = new cron.CronJob('30 22 * * 5', () => {
+//   // console.log("fire now (2)");
+//   console.log("Sending GPQ (2) message");
+//   bot.channels.cache.get(dailyMessageChannelID).send("Hello everyone, we will be starting Guild PQ in **_30 minutes_**! See you all there at Guild HQ Channel 4 (via the Lea NPC in Henesys)!");
+// });
 
 bot.on('message', msg => {
   // if (msg.content == "rawr") {
   //     bot.channels.cache.get(channelID).send("<@&725880768659980330>" + " :triangular_flag_on_post: Flag Race will begin in **_5 minutes_**! :triangular_flag_on_post:");
   // }
   if (msg.content === "!notifs") {
-    if(msg.member.roles.cache.some(r=>["flaggers"].includes(r.name))) {
+    if(msg.member.roles.cache.some(r=>["Flag Race"].includes(r.name))) {
       // console.log(`Yay, the author of the message has the role!`);
-      let role = msg.guild.roles.cache.find(r => r.name === "flaggers");
+      let role = msg.guild.roles.cache.find(r => r.name === "Flag Race");
       const guildMember = msg.member;
       guildMember.roles.remove(role).catch(console.error);
       msg.reply("You will no longer receive Flag Race notifications.");
     } else {
       // console.log(`Nope, noppers, nadda.`);
-      let role = msg.guild.roles.cache.find(r => r.name === "flaggers");
+      let role = msg.guild.roles.cache.find(r => r.name === "Flag Race");
       const guildMember = msg.member;
       guildMember.roles.add(role).catch(console.error);
       msg.reply("You have signed up for Flag Race notifications!");
     }
   }
 
+  // if (msg.content == "!testannounce") {
+  //   bot.channels.cache.get(channelID).send("" + flagRaceRoleID + " Please disregard this message, this is a test announcement!");
+  // }
+
   // if (msg.content === "jae") {
-  //   msg.reply("jaebg");
+  //   bot.channels.cache.get(channelID).send("" + flagRaceRoleID + " :triangular_flag_on_post: Flag Race will begin in **_5 minutes_**! :triangular_flag_on_post:");
   // }
 
   // if (msg.content === "gg") {
@@ -86,7 +93,7 @@ bot.on('message', msg => {
   // }
 
   if (msg.content === "!help" || msg.content === "commands"  && msg.channel.id === botChannelID) {
-    msg.reply("Currently available commands: !flag, !notifs, !ia, !starforce, !flames, !cubing");
+    msg.reply("Currently available commands: !flag, !notifs, !ia, !starforce, !cubing");
   }
 
   if (msg.content === "!ia" && msg.channel.id === botChannelID) {
@@ -97,9 +104,9 @@ bot.on('message', msg => {
     msg.reply("\nFull numbers: <https://strategywiki.org/wiki/MapleStory/Spell_Trace_and_Star_Force> \nCalculator: <https://jsbin.com/bewimaxasa/1> \nExpected cost+boom: <https://bit.ly/2YZT3Fa>");
   }
 
-  if (msg.content === "!flames" && msg.channel.id === botChannelID) {
-    msg.reply("\nGuide: <https://bit.ly/2zHMInA> \nCalculator: <https://output.jsbin.com/tacijeq>");
-  }
+  // if (msg.content === "!flames" && msg.channel.id === botChannelID) {
+  //   msg.reply("\nGuide: <https://bit.ly/2zHMInA> \nCalculator: <https://output.jsbin.com/tacijeq>");
+  // }
 
   if (msg.content === "!cubing" && msg.channel.id === botChannelID) {
     msg.reply("\nCubing Calculator: <https://jsfiddle.net/SuckHard/gp5mjc0v/show> \nCubing Sim: <https://stripedypaper.github.io/cube/>");
@@ -145,35 +152,8 @@ if (msg.content === "!flag" && msg.channel.id === botChannelID) {
 
 });
 
-
-// bot.on('messageReactionAdd', async (reaction, user) => {
-// 	// When we receive a reaction we check if the reaction is partial or not
-// 	if (reaction.partial) {
-// 		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-// 		try {
-// 			await reaction.fetch();
-// 		} catch (error) {
-// 			console.log('Something went wrong when fetching the message: ', error);
-// 			// Return as `reaction.message.author` may be undefined/null
-// 			return;
-// 		}
-//   }
-//   if (reaction.message.id == 725894587813724202) {
-//     console.log("correct");
-//     const member = reaction.message.member;
-//     member.roles.set([])
-//   .then(member => console.log(`Member roles is now of ${member.roles.cache.size} size`))
-//   .catch(console.error);
-// 	// Now the message has been cached and is fully available
-// 	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-// 	// The reaction is now also fully available and the properties will be reflected accurately:
-// 	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-//   }
-// });
-
-// scheduledMessage.start();
-dailyMessage.start();
+// dailyMessage.start();
 flagMessage.start();
-gpqMessage.start();
-gpqMessage2.start();
+// gpqMessage.start();
+// gpqMessage2.start();
 bot.login(token);
